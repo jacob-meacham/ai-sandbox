7dbd232a0153b3bfa236e32469b2502b0a0a73c0
Jacob Meacham <jacob.e.meacham@gmail.com>
2023-07-10 00:27:08 -0600

    Grab OpenAI key

--- embeddings/main.py
+++ embeddings/main.py
@@ -1,5 +1,17 @@
 import streamlit as st
 import pandas as pd
+import os
+
+def get_openai_api_key():
+    api_key = os.environ.get('OPENAI_API_KEY')
+    if not api_key:
+        key_file = os.path.join(os.path.expanduser("~"),
+                                ".openai")
+        api_key = open(key_file).read().strip()
+
+    return api_key
+
+
 df = pd.DataFrame({
       'first column': [1, 2, 3, 4],
       'second column': [10, 20, 30, 40]
